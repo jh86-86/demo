6f7630062a500226c7e61e3df2ce5070dae26e44
JJ <jh_86@hotmail.co.uk>
2021-05-18 10:34:14 +0100

    crud in operation but validation from validator not working correctly. Will finish project and sort this later

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,7 +3,11 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,8 +23,8 @@ public class PersonController {
     }
 
     @PostMapping //makes it an endpoint for post request
-    public void addPerson(@RequestBody Person person){ //take request body, put inside person
-        personService.addPerson(person);
+    public void addPerson(@Valid @Validated @NotNull @RequestBody Person person){ //take request body, put inside person
+        personService.addPerson(person); //not null will fail there rather than in our code
     }
 
     @GetMapping //return all users from db
@@ -29,7 +33,7 @@ public class PersonController {
     }
 
     @GetMapping(path= "{id}") //allows me to have forward slash for the id
-    public Person getPersonId(@PathVariable("id") UUID id){  //turns id into a UUID
+    public Person getPersonId(@PathVariable("id") UUID id){  //turns id into a UUID pathvariable connects to the path varirable mapping abve
         return personService.getPersonById(id)
                 .orElse(null); //could have custom message here could add 404
     }
@@ -40,7 +44,7 @@ public class PersonController {
     }
 
     @PutMapping(path="{id}")
-    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate){ //takes a person from the request bdy
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NotNull @RequestBody Person personToUpdate){ //takes a person from the request bdy
         personService.updatePerson(id, personToUpdate);
     }
 
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,10 +1,13 @@
 package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class Person {
-    private final UUID id;
+    private final UUID id; //UUID gives us a random accpected ID
+
+    @NotBlank //dont want this to be blank, has not be a name, if we used @Notnull it could still be an empty stirng
     private final String name;
 
     public Person(@JsonProperty("id") UUID id,
